ac4523888e777c49f2c907635d9888b61c33c1e0
Kazumasa KUMAMOTO <kumamotone@gmail.com>
2022-02-07 23:03:15 +0900

    bad commit

--- lib/ui/root_page.dart
+++ lib/ui/root_page.dart
@@ -4,6 +4,8 @@ class RootPage extends StatelessWidget {
   const RootPage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    // unused local value
+    var hoge = "a";
     return Scaffold(
       appBar: AppBar(
         title: const Text('App Name'),
